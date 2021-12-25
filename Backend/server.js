3640f2cbce3af97371d0e346a5186bac52e11d6c
shOtb <justshahad@hotmail.com>
2021-12-25 23:36:59 +0300

    finish get and post

--- Backend/server.js
+++ Backend/server.js
@@ -3,8 +3,9 @@ const app=express()
 
 const db=require('./db')
 const Todo=require('./todo')
-
 //console.log(Todo);
+
+app.use(express.json())
 
 
 app.get('/',(req,res)=>{
@@ -12,8 +13,26 @@ app.get('/',(req,res)=>{
 })
 
 app.get('/tasks',(req,res)=>{
-    res.json('GET / is Working')
-})
+    Todo.find({},(err,data)=>{
+        if (err) {
+            console.log('ERROR:',err);
+        } else {
+            res.json(data);
+        }
+    });
+});
+
+app.post('/tasks',(req,res)=>{
+    console.log('25:',req.body)
+
+    Todo.create(req.body ,(err,newTask)=>{
+        if (err) {
+            console.log('ERROR:',err);
+        } else {
+            res.status(201).json(newTask);
+        }
+    });
+});
 
 app.listen(5000,()=>{
     console.log('SERVER IS WORKING...');
